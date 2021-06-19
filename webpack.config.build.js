c2b8256e77497bbedf7dbe377a6fe8f98e89b97d
Dan Soutar <dan.soutar@littlerocket.io>
2021-06-19 11:08:02 -0400

    first commit - configuring webpack

---
+++ webpack.config.build.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const { merge } = require('webpack-merge');
+const config = require('./webpack.config');
