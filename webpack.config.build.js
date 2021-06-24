8ece03e11a3fe7de324388a90f1df05f0948fb8e
Dan Soutar <dan.soutar@littlerocket.io>
2021-06-24 00:52:19 -0400

    webpack boilerplate - v1

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,13 @@
-const path = require('path');
+const path = require('path')
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
 
-const { merge } = require('webpack-merge');
-const config = require('./webpack.config');
+module.exports = merge(config, {
+  mode: 'production',
+
+  output: {
+    path: path.join(__dirname, 'public')
+  }
+
+})
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,18 +1,18 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
+const { merge } = require('webpack-merge')
+const path = require('path')
 
-const config = require('./webpack.config');
+const config = require('./webpack.config')
 
 module.exports = merge(config, {
-    mode: 'development',
-    
-    devtool: 'inline-source-map',
+  mode: 'development',
 
-    devServer: {
-        writeToDisk: true,
-    },
+  devtool: 'inline-source-map',
 
-    output: {
-        path: path.join(__dirname, 'public')
-    }
-});
+  devServer: {
+    writeToDisk: true
+  },
+
+  output: {
+    path: path.join(__dirname, 'public')
+  }
+})
